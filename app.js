6968f071855e89e023bb4e19f2c7c213f79b8f12
Kaku0201 <kakurim0201@gmail.com>
2026-04-24 14:16:36 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("raid sim loaded");
+document.querySelectorAll(".spot").forEach(btn=>{
+ btn.addEventListener("click",()=>{
+   alert("You selected "+btn.innerText);
+ });
+});
+
+document.getElementById("generate")
+.addEventListener("click",()=>{
+ alert("Random mechanic later!");
+});
